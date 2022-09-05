e474ccfef4e80af7bcf09c4394360ad62a13e2e8
Thallys <thallysvilemos@gmail.com>
2022-09-04 23:43:25 -0300

    Alteração da lógica de exibição de animação ao pesquisar na tabela

--- src/app/dashboard/table/table.component.ts
+++ src/app/dashboard/table/table.component.ts
@@ -1,10 +1,9 @@
-import { debounceTime, distinctUntilChanged, filter, merge, Observable, switchMap, tap, map } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, merge, switchMap, tap, map } from 'rxjs';
 import { VehicleDataService } from './vehicleData.service';
 import { environment } from './../../../environments/environment';
-import { Component, OnInit } from '@angular/core';
+import { Component, } from '@angular/core';
 import { vehicleDataArray } from './vehicleData';
 import { FormControl } from '@angular/forms';
-
 
 const API = environment.apiURL
 @Component({
@@ -12,41 +11,27 @@ const API = environment.apiURL
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit {
-  // input = ""
-  dados!: any //dados de vehicle data que serão exibidos na tabela
-  vehicleDataArray!: vehicleDataArray
+export class TableComponent {
+  dados!: vehicleDataArray //dados de vehicleData que serão exibidos na tabela
 
   vehicleDataInput = new FormControl();
-  veiculosArray$ = this.vehicleData.getVeiculos().pipe(
-    tap(()=>{console.log('Fluxo Inicial')})
-  )
+  veiculosArray$ = this.vehicleData.getVeiculos();
   filtroPorInput$ = this.vehicleDataInput.valueChanges.pipe(
     debounceTime(300),
-    tap(()=>{console.log('Fluxo Filtro')}),
     tap(console.log),
-    filter((valorDigitado)=> valorDigitado.length >= 19 || !valorDigitado.length),
+    filter((valorDigitado)=> valorDigitado.length >= 17 || !valorDigitado.length),
     distinctUntilChanged(),
     switchMap((valorDigitado)=> this.vehicleData.getVeiculos(valorDigitado)),
     tap(console.log)
-  )
-  veiculos$ = merge(this.filtroPorInput$, this.veiculosArray$)
+  );
+
+  veiculos$ = merge(this.filtroPorInput$, this.veiculosArray$);
 
   constructor(private vehicleData: VehicleDataService) { }
 
-  ngOnInit() {// a rimeira busca será feita ao carregar a página para a tabela n iniciar vazia
-  // this.results$ = this.vehicleDataInput.valueChanges.pipe(tap(value => console.log(value)))
-  this.retornaDados()
-  // this.vehicleData.getVehicleDataApi().subscribe((retornoApi)=>{this.vehicleDataArray = retornoApi.vehicledata })
-  }
-
-  retornaDados() { //método será chamado ao carregar a página e ao mudar o campo de seleção da tabela
+  //método será chamado ao preencher o input com a quantidade de caracteres definidos no filtroPorInput
+  retornaDados() {
     return this.vehicleData.getVeiculos().subscribe({next:(data)=>{this.dados = data},
-    error:(error)=>{console.log(error)}})
+    error:(error)=>{console.log(error)}});
   }
-  // onSearch() {
-  //   console.log(this.vehicleDataInput);
-  //   return this.filtroPorInput$, this.veiculos$
-  //   console.log(this.input)
-  // }
 }
--- src/app/dashboard/table/vehicleData.service.ts
+++ src/app/dashboard/table/vehicleData.service.ts
@@ -3,40 +3,32 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { vehicleData, vehicleDataAPI } from './vehicleData';
 import { map, tap } from 'rxjs';
-const API = environment.apiURL
+
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root'
 })
-// Table de vehicleData
+
 export class VehicleDataService {
 
   constructor(private http: HttpClient) { }
 
-  // getVehicledataById(codigoVn: string) {//Busca veiculo por id associado ao código vn no template
-  //   return this.http.get<Table>(`${API}/vehicledata/${codigoVn}`)
-  // }
-  // getVehicleDataApi(){
-  //   return this.http.get<TabelaAPI>(`${API}/vehicleData`)
-    // .pipe(
-    //   tap((valor)=> console.log(valor)),
-    //   map((data)=> data.vehicledata.sort((veiculoA, veiculoB) => this.ordenaPorCodigo(veiculoA, veiculoB))))
-  // }
   getVeiculos(valor?: string){
     const params = valor ? new HttpParams().append('valor', valor) : undefined;
     return this.http.get<vehicleDataAPI>(`${API}/vehicleData`, { params }).pipe(
-      tap((valor)=>console.log(valor.vehicleData.length)),
       map((veiculos)=>
-      veiculos.vehicleData.sort((veiculoA, veiculoB)=> this.ordenaPorCodigo(veiculoA, veiculoB)))
-    )
+      veiculos.vehicleData.sort((veiculoA, veiculoB)=> this.ordenaPorId(veiculoA, veiculoB)))
+    );
   }
-  private ordenaPorCodigo(veiculoA: vehicleData, veiculoB: vehicleData) {
+
+  private ordenaPorId(veiculoA: vehicleData, veiculoB: vehicleData) {
     if(veiculoA.id > veiculoB.id){
-      return 1
+      return 1;
     }
     if(veiculoA.id < veiculoB.id){
-      return -1
+      return -1;
     }
-    return 0
+    return 0;
   }
 }
